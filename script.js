543ad52c2bef3643093686e2744d50d1a50f35cb
estjhohansbello-unimilitar <est.jhohans.bello@unimilitar.edu.co>
2026-05-08 09:39:45 -0500

    Se define el area de dibujo, const algoritmo y casos de prueba en script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+// canvas 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//constantes de la region
+const INSIDE = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const BOTTOM = 4;
+const TOP = 8;
+// visualizacion del viewport 
+let xmin = 150;
+let ymin = 100;
+let xmax = 450;
+let ymax = 300;
+//Escenas 
+const escenas = [
+
+    {
+        nombre: "Caso 1: Línea completamente dentro",
+        x1: 200,
+        y1: 150,
+        x2: 350,
+        y2: 250
+    },
+
+    {
+        nombre: "Caso 2: Línea completamente fuera",
+        x1: 20,
+        y1: 20,
+        x2: 80,
+        y2: 70
+    },
+
+    {
+        nombre: "Caso 3: Línea entrando por la izquierda",
+        x1: 50,
+        y1: 180,
+        x2: 300,
+        y2: 180
+    },
+
+    {
+        nombre: "Caso 4: Línea entrando por la derecha",
+        x1: 300,
+        y1: 220,
+        x2: 600,
+        y2: 220
+    },
+
+    {
+        nombre: "Caso 5: Línea atravesando viewport",
+        x1: 50,
+        y1: 50,
+        x2: 600,
+        y2: 350
+    }
+];
+
+let indiceEscena = 0;
